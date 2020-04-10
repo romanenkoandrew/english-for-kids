e5c14e2c79e87dde38ffa1d09742002cfa80e8a2
RomanenkoAndrew <romanenkoandrey95@mail.ru>
2020-04-10 10:49:39 +0300

    fix: switch-mode

--- src/scripts/header-menu/switchMode.js
+++ src/scripts/header-menu/switchMode.js
@@ -1,22 +1,14 @@
 import data from '../data'
 
 const switchMode = () => {
-  const switchModeContainer = document.getElementById('switchModeContainer')
-  const switchModeHandler = document.querySelector('.switch-handler')
-  // const switchModeText = document.querySelector('.switch-mode-text')
-  switchModeContainer.addEventListener('click', () => {
-    if(!data.game) {
-      switchModeHandler.style.animationName = 'switch-handler-left'
-      // switchModeText.before.style.opacity = '1'
+  const switchModeLabel = document.getElementById('switchMode')
+
+  switchModeLabel.addEventListener('click', () => {
+    if (!data.game) {
       data.game = true
     } else {
-      switchModeHandler.style.animationName = 'switch-handler-right'
-      // switchModeText.before.style.opacity = '1'
       data.game = false
-
     }
-
-    console.log(data.game)
   })
 }
 export default switchMode
